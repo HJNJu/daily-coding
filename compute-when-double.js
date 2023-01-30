0bbde927de59d0537965ffa0f6e4b95a300b6635
Hyojin <hyojin.ju@gmx.net>
2023-01-30 19:08:28 +0900

    Double principal with given interest rate

---
+++ compute-when-double.js
@@ -0,0 +1,13 @@
+// 연이율을 입력받아 원금이 2배 이상이 될 때까지 걸리는 시간(년)을 리턴.
+
+function computeWhenDouble(interestRate) {
+    let rate = 1 + interestRate / 100;
+    let principal = 1;
+    let totalYear = 0;
+    
+    while (principal < 2) {
+      principal = principal * rate;
+      totalYear++;
+    }
+    return totalYear;
+  }
