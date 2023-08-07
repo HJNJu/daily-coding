8e83a4fc4fe1dfeb901118424c24ef4eb76b9d2d
Hyojin Ju <hyojin.ju@gmx.net>
2023-08-07 20:27:45 +0900

    Decipher the morse code

---
+++ morse-code-1.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    };
+    let letter_arr = letter.split(' ')
+    var answer = '';
+    for (let i = 0; i < letter_arr.length; i++) {
+        answer += morse[letter_arr[i]]
+    }
+    
+    return answer;
+}
